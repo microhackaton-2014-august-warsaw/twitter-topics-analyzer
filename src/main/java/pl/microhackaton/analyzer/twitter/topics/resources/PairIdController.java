49c90605ff677c763f6ad597ea6f426b6cb59c23
hexmind <hexmind@hexmind.pl>
2014-08-09 17:21:35 +0200

    api

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
@@ -1,30 +1,36 @@
 package pl.microhackaton.analyzer.twitter.topics.resources;
 
-import com.codahale.metrics.annotation.Timed;
-
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import pl.microhackaton.analyzer.twitter.topics.model.AnalyzeTweetsRequest;
 import pl.microhackaton.analyzer.twitter.topics.service.AnalyzeRunner;
 
+import com.codahale.metrics.annotation.Timed;
+
 /**
  * Created by pmasko on 09.08.2014.
  */
-@Path("/api/{pairId}")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PairIdController {
 
 	@GET
 	@Timed
-	public String sayHello(@PathParam("pairId") String pairId) {
-		return "it works!";
+	@Path("/api/hello/{name}")
+	public String sayHello(@PathParam("name") String name) {
+		return "it works, " + name;
 	}
 
 	@POST
 	@Timed
-	public void analyze(AnalyzeTweetsRequest analyzeTweetsRequest) {
+	@Path("/api/{pairId}")
+	public void analyze(AnalyzeTweetsRequest analyzeTweetsRequest, @PathParam("pairId") String pairId) {
 		AnalyzeRunner analyzer = new AnalyzeRunner(analyzeTweetsRequest);
 		analyzer.run();
 	}
