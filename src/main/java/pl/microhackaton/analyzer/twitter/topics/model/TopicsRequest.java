ae48f6c81fbdec7478e28046fe8642062154d7fb
hexmind <hexmind@hexmind.pl>
2014-08-09 15:15:27 +0200

    model

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/model/TopicsRequest.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/model/TopicsRequest.java
@@ -2,10 +2,14 @@ package pl.microhackaton.analyzer.twitter.topics.model;
 
 import java.util.List;
 
-public class TopicsResponse {
+public class TopicsRequest {
+
+	private static final String DATA_SOURCE = "twitter";
 
 	private long pairId;
-	private List<TopicInfo> topics;
+	private String analyzerType = DATA_SOURCE;
+	private String analyzedId;
+	private List<Topic> topics;
 
 	public long getPairId() {
 		return pairId;
@@ -15,11 +19,27 @@ public class TopicsResponse {
 		this.pairId = pairId;
 	}
 
-	public List<TopicInfo> getTopics() {
+	public String getAnalyzerType() {
+		return analyzerType;
+	}
+
+	public void setAnalyzerType(String analyzerType) {
+		this.analyzerType = analyzerType;
+	}
+
+	public String getAnalyzedId() {
+		return analyzedId;
+	}
+
+	public void setAnalyzedId(String analyzedId) {
+		this.analyzedId = analyzedId;
+	}
+
+	public List<Topic> getTopics() {
 		return topics;
 	}
 
-	public void setTopics(List<TopicInfo> topics) {
+	public void setTopics(List<Topic> topics) {
 		this.topics = topics;
 	}
 }
--- src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
@@ -5,18 +5,28 @@ import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import pl.microhackaton.analyzer.twitter.topics.model.AnalyzeTweetsRequest;
+import pl.microhackaton.analyzer.twitter.topics.service.AnalyzeRunner;
+
 /**
  * Created by pmasko on 09.08.2014.
  */
 @Path("/api/{pairId}")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PairIdController {
 
-    @GET
-    @Timed
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String sayHello(@PathParam("pairId") String pairId) {
-        return "it works!";
-    }
+	@GET
+	@Timed
+	public String sayHello(@PathParam("pairId") String pairId) {
+		return "it works!";
+	}
+
+	@POST
+	@Timed
+	public void analyze(AnalyzeTweetsRequest analyzeTweetsRequest) {
+		AnalyzeRunner analyzer = new AnalyzeRunner(analyzeTweetsRequest);
+		analyzer.run();
+	}
 
 }
