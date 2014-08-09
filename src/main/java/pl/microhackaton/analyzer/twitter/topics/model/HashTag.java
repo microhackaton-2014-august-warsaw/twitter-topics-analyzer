cf7e56c22a377ed036d7e41bbf4a0d64c00cbb9f
Rafal Remisiewicz <rafal.remisiewicz@gmail.com>
2014-08-09 15:36:11 +0200

    json mapping

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/model/HashTag.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/model/HashTag.java
@@ -1,11 +1,23 @@
 package pl.microhackaton.analyzer.twitter.topics.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.common.base.Objects;
+import com.google.common.hash.HashCode;
+
 /**
  * Created by rafal on 09.08.14.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class HashTag {
 
     private String text;
+
+    public HashTag() {
+    }
+
+    public HashTag(String text) {
+        this.text = text;
+    }
 
     public String getText() {
         return text;
@@ -13,5 +25,16 @@ public class HashTag {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(text);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        HashTag that = (HashTag) obj;
+        return Objects.equal(text, that.text);
     }
 }
