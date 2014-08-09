b9a25acf3b9eda073b19a0f031929b58494fddb7
pmasko <pmasko@gmail.com>
2014-08-09 13:31:15 +0200

    First working resource

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/PairIdController.java
@@ -3,10 +3,7 @@ package pl.microhackaton.analyzer.twitter.topics.resources;
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -18,6 +15,7 @@ public class PairIdController {
     @GET
     @Timed
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public String sayHello(@PathParam("pairId") String pairId) {
         return "it works!";
     }
