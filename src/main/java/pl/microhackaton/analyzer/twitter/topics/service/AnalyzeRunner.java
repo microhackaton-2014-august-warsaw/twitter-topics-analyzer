b78889f569998aa62a3187d10d1c32e4196b8b6b
hexmind <hexmind@hexmind.pl>
2014-08-09 15:49:10 +0200

    analyze

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/service/AnalyzeRunner.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/service/AnalyzeRunner.java
@@ -1,17 +1,34 @@
 package pl.microhackaton.analyzer.twitter.topics.service;
 
 import pl.microhackaton.analyzer.twitter.topics.model.AnalyzeTweetsRequest;
+import pl.microhackaton.analyzer.twitter.topics.model.HashTag;
+import pl.microhackaton.analyzer.twitter.topics.model.Topic;
+import pl.microhackaton.analyzer.twitter.topics.model.TopicsRequest;
+import pl.microhackaton.analyzer.twitter.topics.model.Tweet;
 
 public class AnalyzeRunner implements Runnable {
 
-	private final AnalyzeTweetsRequest analyzeTweetsRequest;
+	private final AnalyzeTweetsRequest tweetsData;
 
 	public AnalyzeRunner(AnalyzeTweetsRequest analyzeTweetsRequest) {
-		this.analyzeTweetsRequest = analyzeTweetsRequest;
+		this.tweetsData = analyzeTweetsRequest;
 	}
 
 	@Override
 	public void run() {
-		// TODO analyzeTweetsRequest > topicsRequest > correlator
+		TopicsRequest topicsRequest = createTopicsRequest();
+		// TODO call correlator
+	}
+
+	private TopicsRequest createTopicsRequest() {
+		TopicsRequest topicsData = new TopicsRequest();
+		topicsData.setAnalyzedId(tweetsData.getTwitterLogin());
+		for (Tweet tweet : tweetsData.getTweets()) {
+			for (HashTag tag : tweet.getEntities().getHashTags()) {
+				Topic topic = new Topic(tag.getText());
+				topicsData.getTopics().add(topic);
+			}
+		}
+		return topicsData;
 	}
 }
