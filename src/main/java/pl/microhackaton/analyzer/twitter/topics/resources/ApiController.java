82c0307924bf036026071eb2342e1f7f03aa7b5d
hexmind <hexmind@hexmind.pl>
2014-08-09 19:19:09 +0200

    api

--- src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/ApiController.java
+++ src/main/java/pl/microhackaton/analyzer/twitter/topics/resources/ApiController.java
@@ -1,7 +1,11 @@
 package pl.microhackaton.analyzer.twitter.topics.resources;
 
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -35,24 +39,33 @@ public class ApiController {
     @GET
 	@Timed
 	@Path("/hello/{name}")
-	public String sayHello(@PathParam("name") String name) {
+	public String getHello(@PathParam("name") String name) {
 		return "{\"hello\" : \"" + name + "\"}";
 	}
 
 	@POST
 	@Timed
+	@Path("/hello2/{name}")
+	public String postGello(String name) {
+		return getHello(name);
+	}
+
+	@POST
+	@Timed
 	@Path("/tweets/{twitterLogin}/{pairId}")
-	public void analyze(Set<Tweet> tweets,
+	public String analyze(Set<Tweet> tweets,
 			@PathParam("twitterLogin") String twitterLogin,
-			@PathParam("pairId") Long pairId) {
+			@PathParam("pairId") Long pairId) throws InterruptedException,
+			ExecutionException, TimeoutException {
 		AnalyzeTweetsRequest request = new AnalyzeTweetsRequest();
 		request.setTweets(tweets);
 		request.setTwitterLogin(twitterLogin);
 		request.setPairId(pairId);
 
-		AnalyzeRunner analyzer = new AnalyzeRunner(request, serviceResolver.getUrl("common-topics-correlator"));
+		AnalyzeRunner analyzer = new AnalyzeRunner(request, null);
+		Executors.newCachedThreadPool().submit(analyzer);
 
-		Executors.newCachedThreadPool().execute(analyzer);
+		return "{}";
 	}
 
 }
